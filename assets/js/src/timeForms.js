1755c9e0ea21432661b0d18bd749e926567fdcf0
Danila Grobov <danila.grob@gmail.com>
2018-12-07 10:57:32 +0200

    Pakeiciau,kaip atrodo senas laikas

--- assets/js/src/timeForms.js
+++ assets/js/src/timeForms.js
@@ -1,40 +1,48 @@
 const getNewForm = ( ) =>
 {
 
-    const formTime = '<form class="formTime mb-4 timeAdults">\n' +'<div class="closeButton"><i class="fas fa-times"></i></div>'+
-        '                    <div class="form-group col-md-12">\n' +
-        '                        <label for="days">Type</label>\n' +
-        '                        <select class="form-control Type">\n' +
-        '                            <option>Kids</option>\n' +
-        '                            <option .disabledFormselected>Adults</option>\n' +
-        '                        </select>\n' +
-        '                    </div>\n' +
-        '                    <div class="form-group col-md-12">\n' +
-        '                        <label for="timepicker">From</label>\n' +
-        '                        <input type="text" name="timepicker"  placeholder="12:00" class="form-control From" data-timepicker="" autocomplete="off">\n' +
-        '                    </div>\n' +
-        '                    <div class="form-group col-md-12">\n' +
-        '                        <label for="timepicker">To</label>\n' +
-        '                        <input type="text" name="timepicker"  placeholder="12:00" class="form-control To" data-timepicker="" autocomplete="off">\n' +
-        '                    </div>\n' +
-        '                    <div class="form-group col-md-12">\n' +
-        '                    <div class="input-group mb-3">\n' +
-        '                        <div class="input-group-prepend">\n' +
-        '                            <div class="input-group-text">\n' +
-        '                                <input class="repeatEveryCheckbox" type="checkbox" aria-label="Checkbox for following text input">\n' +
-        '                                <span class="" id="">&nbsp;&nbsp; repeat for </span>\n' +
-        '                            </div>\n' +
-        '                        </div>\n' +
-        '                        <input type="number" min="1" class="repeatEveryInput form-control" value="1" aria-label="Text input with checkbox">\n' +
-        '                        <div class="input-group-append">\n' +
-        '                            <div class="input-group-text">\n' +
-        '                                <span class="" id=""> week(s) </span>\n' +
-        '                            </div>\n' +
-        '                        </div>\n' +
-        '                    </div>\n' +
-        '                    </div>\n' +
-        '                </form>';
-    return formTime;
+    return `
+    <form class="formTime mb-4 timeAdults">
+         <div class="topButtons topButtons--old">
+            <div class="closeButton"><i class="fas fa-times"></i></div>
+        </div>
+        <div class="form-group col-md-12">
+            <label for="days">Type</label>
+            <select class="form-control Type">
+                <option>Kids</option>
+                <option selected>Adults</option>
+            </select>
+        </div>
+        <div class="form-group col-md-12">
+            <label for="timepicker">From</label>
+            <input type="text" name="timepicker" placeholder="12:00" class="form-control From" data-timepicker=""
+                   autocomplete="off">
+        </div>
+        <div class="form-group col-md-12">
+            <label for="timepicker">To</label>
+            <input type="text" name="timepicker" placeholder="12:00" class="form-control To" data-timepicker=""
+                   autocomplete="off">
+        </div>
+        <div class="form-group col-md-12">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">
+                        <input class="repeatEveryCheckbox" type="checkbox" aria-label="Checkbox for following text input">
+                        <span class="" id="">&nbsp;&nbsp; repeat for </span>
+                    </div>
+                </div>
+                <input type="number" min="1" class="repeatEveryInput form-control" value="1"
+                       aria-label="Text input with checkbox">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="" id=""> week(s) </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    `;
+
 };
 const getOldForm = (type,from, to) =>
 {
@@ -50,41 +58,50 @@ const getOldForm = (type,from, to) =>
         formType = 'timeAdults';
     }
     return`
-    <form class="disabledForm formTime mb-4 ${formType}">
-    <div class="topButtons">
-        <div class="closeButton"><i class="fas fa-times"></i></div>
-    </div>
-    <div class="bottomButtons">
+    <form class="formTime mb-4 ${formType}">
+        <div class="topButtons topButtons--old">
+            <div class="closeButton"><i class="fas fa-times"></i></div>
+        </div>
+        
+        <div class="bottomButtons">
+            <div class="form-group col-md-12">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="customCheck2">
+                    <label class="custom-control-label" for="customCheck2">Edit</label>
+                </div>
+            </div>
+        </div>
+        
         <div class="form-group col-md-12">
-        <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="customCheck2">
-            <label class="custom-control-label" for="customCheck2">Edit</label>
+            <label for="days">Type</label>
+            <select class="form-control Type">
+                <option ${Kids}>Kids</option>
+                <option ${Adults}>Adults</option>
+            </select>
         </div>
-    </div>
-    </div>
-    <div class="form-group col-md-12">
-        <label for="days">Type</label>
-        <select class="form-control Type">
-            <option ${Kids}>Kids</option>
-            <option ${Adults}>Adults</option>
-        </select>
-    </div>
-    <div class="form-group col-md-12">
-        <label for="timepicker">From</label>
-        <input type="text" name="timepicker" value=" ${from} " placeholder="12:00" class="form-control From"
-               data-timepicker="" autocomplete="off">
-    </div>
-    <div class="form-group col-md-12">
-        <label for="timepicker">To</label>
-        <input type="text" name="timepicker" value=" ${to}" placeholder="12:00" class="form-control To"
-               data-timepicker="" autocomplete="off">
-    </div>
-    <div class="form-group col-md-12">
-        <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="customCheck1">
-            <label class="custom-control-label" for="customCheck1">Apply for all</label>
+        
+        <div class="form-group col-md-12">
+            <label for="timepicker">From</label>
+            <input type="text" name="timepicker" value=" ${from} " placeholder="12:00" class="form-control From"
+                   data-timepicker="" autocomplete="off">
         </div>
-    </div>
+        
+        <div class="form-group col-md-12">
+            <label for="timepicker">To</label>
+            <input type="text" name="timepicker" value=" ${to}" placeholder="12:00" class="form-control To"
+                   data-timepicker="" autocomplete="off">
+        </div>
+        
+        <div class="DisableOverlay">
+        </div>
+        
+        <div class="form-group col-md-12 ApplyForAll">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Apply for all</label>
+            </div>
+        </div>
+    
     </form>
     `;
 
