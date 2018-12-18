7473cefb33fb01366fde48256b7b8a07250ba449
Danila Grobov <danila.grob@gmail.com>
2018-12-19 00:20:28 +0200

    pakeistas registraciju puslapis

--- assets/js/src/registrations.js
+++ assets/js/src/registrations.js
@@ -1,32 +1,4 @@
 $(()=>{
-    var random = function random() {
-        return Math.round(Math.random() * 100);
-    };
-
-    const lineChart = new Chart($('.RegistrationChart'), {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [{
-                label: 'Succesfull',
-                backgroundColor: 'rgba(75,181,67, 0.2)',
-                borderColor: 'rgba(75,181,67, 1)',
-                pointBackgroundColor: 'rgba(75,181,67, 1)',
-                pointBorderColor: '#fff',
-                data: [random(), random(), random(), random(), random(), random(), random()]
-            }, {
-                label: 'Cancelled',
-                backgroundColor: 'rgba(166,50,50, 0.2)',
-                borderColor: 'rgba(166,50,50, 1)',
-                pointBackgroundColor: 'rgba(166,50,50, 1)',
-                pointBorderColor: '#fff',
-                data: [random(), random(), random(), random(), random(), random(), random()]
-            }]
-        },
-        options: {
-            responsive: true
-        }
-    });
     $('#table_id').DataTable();
     $('.CancelButton').click((e)=>{
         const id = parseInt(($(e.currentTarget).parents('tr').attr('id') + "").replace('registration-',''));
@@ -55,4 +27,7 @@ $(()=>{
             }
         });
     });
+    $('.Comment').each((el)=>{
+        console.log($(el).html())
+    })
 });
